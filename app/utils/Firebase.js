6e469bd79579ec4fe2d6ad74a73d811dd916fa05
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-11-21 12:25:10 +0100

    firebase error handling

--- app/utils/Firebase.js
+++ app/utils/Firebase.js
@@ -3,19 +3,37 @@ import firebase from 'react-native-firebase';
 class ZadokaFirebase {
 
     async getZadokaPath(zadokaDay) {
-        const doc = await firebase.firestore().doc('daily/' + zadokaDay).get();
-        return doc.get('path');
+        let path = undefined;
+        try {
+            const doc = await firebase.firestore().collection('daily').doc(zadokaDay).get();
+            if(doc.exist) {
+                path = doc.get('path');            
+            }   
+        } catch (error) {
+            console.error(error);            
+        }   
+
+        return path;
     }
 
     async getImageUrl(path) {
-        const ref = firebase.storage().ref(path);
-        const url = await ref.getDownloadURL();
+        let url = undefined;
+        try {
+            const ref = firebase.storage().ref(path);
+            url = await ref.getDownloadURL();            
+        } catch (error) {
+            console.error(error);
+        }
         return url;
     }
 
     async getZadokaUrl(zadokaDay) {
+        let url = undefined;
         const path = await this.getZadokaPath(zadokaDay);
-        return await this.getImageUrl(path);
+        if(path) {
+            url = await this.getImageUrl(path);
+        }
+        return url;
     }
 }
 
