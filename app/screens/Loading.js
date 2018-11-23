f8c2a73e8c7ba45d7b358ade6f50d68822adcfcd
Arnaud Ehresmann <arnaud.ehresmann@gmail.com>
2018-11-23 14:36:45 +0100

    loading indicator

--- app/screens/Loading.js
+++ app/screens/Loading.js
@@ -3,6 +3,17 @@ import { View, Text, ActivityIndicator, StyleSheet, ImageBackground, Dimensions 
 import firebase from 'react-native-firebase';
 import ImageLoader from '../utils/ImageLoader';
 import * as DateHelper from '../utils/Date';
+import {
+    BallIndicator,
+    BarIndicator,
+    DotIndicator,
+    MaterialIndicator,
+    PacmanIndicator,
+    PulseIndicator,
+    SkypeIndicator,
+    UIActivityIndicator,
+    WaveIndicator,
+  } from 'react-native-indicators';
 
 const { height, width } = Dimensions.get('window');
 
@@ -19,8 +30,11 @@ export default class Loading extends React.Component {
     render() {
         return (
         <View style={styles.container}>
-        <ImageBackground resizeMethod='scale' source={require('../assets/splash.png')} style={{width: width, height: height}}>
-            <ActivityIndicator size="large" />
+        <ImageBackground 
+            resizeMode='contain' 
+            source={require('../assets/splash.png')} 
+            style={styles.background}>
+            <PacmanIndicator size={54} color='black' />
         </ImageBackground>
         </View>
         )
@@ -32,5 +46,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  background: {
+    flex: 1, 
+    width: width, 
+    height: height,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    marginBottom: 20,
   }
 })
